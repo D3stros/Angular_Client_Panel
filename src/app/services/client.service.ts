647a12ed156711d118ffe533fc14d748456fac44
D3stros <patrick.fallmann@hotmail.com>
2019-10-27 15:24:49 +0100

    Implemented Client Balance Update and Edit&Delete clients functionality

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -58,4 +58,14 @@ export class ClientService {
 
     return this.client;
   }
+
+  updateClient(client: Client) {
+    this.clientDoc = this.afs.doc(`clients/${client.id}`);
+    this.clientDoc.update(client);
+  }
+
+  deleteClient(client: Client) {
+    this.clientDoc = this.afs.doc(`clients/${client.id}`);
+    this.clientDoc.delete();
+  }
 }
